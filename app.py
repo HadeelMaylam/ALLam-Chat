1ea2c1aaeae196e7a14ccbc716803e0b328e3068
Hadeel Almaylam <128952588+HadeelMaylam@users.noreply.github.com>
2025-04-26 22:27:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from streamlit_chat import message
 
 API_URL = "https://api.groq.com/openai/v1/chat/completions"
 API_KEY = st.secrets["GROQ_API_KEY"]
+
 headers = {
     "Authorization": f"Bearer {API_KEY}",
     "Content-Type": "application/json"
 }
 
-st.title("علّام الزلفاوي")
+st.title("علّام الغبي")
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -20,15 +22,20 @@ if user_input := st.chat_input("جربني"):
     st.session_state.messages.append({"role": "user", "content": user_input})
     message(user_input, is_user=True, key=f"user_{len(st.session_state.messages)}")
 
-    prompt = f" اجيب على الاسئله التاليه: {user_input}"
+    # تجهيز المحادثة كاملة مع آخر سؤال
+    prompt = f"اجب على الاسئله التاليه: {user_input}"
+    chat_history = st.session_state.messages.copy()
+    chat_history[-1] = {"role": "user", "content": prompt}
+
     payload = {
         "model": "allam-2-7b",
-        "messages": [{"role": "user", "content": prompt}],
+        "messages": chat_history,
         "max_tokens": 100,
         "temperature": 0.6,
     }
 
     res = requests.post(API_URL, headers=headers, json=payload)
     bot_reply = res.json()["choices"][0]["message"]["content"] if res.ok else f"خطأ: {res.status_code}"
-    st.session_state.messages.append({"role": "bot", "content": bot_reply})
+
+    st.session_state.messages.append({"role": "assistant", "content": bot_reply})
     message(bot_reply, key=f"bot_{len(st.session_state.messages)}")
